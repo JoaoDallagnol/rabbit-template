980338793bdea66d6e7644c762bd87fed81c20f6
JoaoDallagnol <jvdallagnol2001@gmail.com>
2026-04-28 10:49:30 -0300

    Creating repo and initial template

---
+++ src/main/java/com/example/rabbit_template/RabbitTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbit_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rabbit_template/RabbitTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbit_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
